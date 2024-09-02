935aebb26071dd6caaa020e002964a978f47e0c4
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-09-03 00:14:32 +0900

    コントローラーをシンプルにするためのリファクタリングをしました。

--- app.rb
+++ app.rb
@@ -5,17 +5,37 @@ require 'sinatra/reloader'
 require 'json'
 require 'pg'
 
-conn ||= PG.connect(dbname: 'my_memo_db')
-conn.exec('CREATE TABLE IF NOT EXISTS memos(id SERIAL PRIMARY KEY, title TEXT NOT NULL, content TEXT NOT NULL)')
+CONN ||= PG.connect(dbname: 'my_memo_db')
+CONN.exec('CREATE TABLE IF NOT EXISTS memos(id SERIAL PRIMARY KEY, title TEXT NOT NULL, content TEXT NOT NULL)')
 
 helpers do
   def escape_html(text)
     Rack::Utils.escape_html(text)
   end
+
+  def load_memos
+    CONN.exec('SELECT * FROM memos')
+  end
+
+  def create_memo(title, content)
+    CONN.exec_params('INSERT INTO memos (title, content) VALUES($1, $2)', [title, content])
+  end
+
+  def find_memo(id)
+    CONN.exec_params('SELECT * FROM memos WHERE id = $1', [id])
+  end
+
+  def update_memo(title, content, id)
+    CONN.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [title, content, id])
+  end
+
+  def delete_memo(id)
+    CONN.exec_params('DELETE FROM memos WHERE ID = $1', [id])
+  end
 end
 
 get '/' do
-  @memos = conn.exec('SELECT * FROM memos')
+  @memos = load_memos
   erb :index
 end
 
@@ -24,17 +44,13 @@ get '/new' do
 end
 
 post '/create' do
-  title = params[:title]
-  content = params[:content]
-
-  conn.exec_params('INSERT INTO memos (title, content) VALUES($1, $2)', [title, content])
+  create_memo(params[:title], params[:content])
 
   redirect '/'
 end
 
 get '/memos/:id' do
-  id = params[:id]
-  memos = conn.exec_params('SELECT * FROM memos WHERE id = $1', [id])
+  memos = find_memo(params[:id])
 
   @title = memos[0]['title']
   @content = memos[0]['content']
@@ -42,28 +58,23 @@ get '/memos/:id' do
 end
 
 get '/memos/:id/edit' do
-  id = params[:id]
-  memos = conn.exec_params('SELECT * FROM memos WHERE id = $1', [id])
+  memos = find_memo(params[:id])
 
-  @id = id
+  @id = params[:id]
   @title = memos[0]['title']
   @content = memos[0]['content']
   erb :edit
 end
 
 patch '/memos/:id' do
-  id = params[:id]
-  title = params[:title]
-  content = params[:content]
+  update_memo(params[:title], params[:content], params[:id])
 
-  conn.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [title, content, id])
-
-  redirect "/memos/#{id}"
+  redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-  id = params[:id]
-  conn.exec_params('DELETE FROM memos WHERE ID = $1', [id])
+  delete_memo(params[:id])
+
   redirect '/'
 end
 
